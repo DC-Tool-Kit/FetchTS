6df04766575103be361258b6b398678aaaf9a8c9
Devin Catuns <deecat10@gmail.com>
2023-01-13 18:25:16 -0600

    create types for yt

---
+++ src/youtube/index.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://www.googleapis.com/youtube/v3/";
+export const WATCH_URL = "https://www.youtube.com/watch?v=";
+export * from "./types";
---
+++ src/youtube/types.ts
@@ -0,0 +1,47 @@
+export type query = string;
+export type Endpoint = "videos" | "search" | string;
+
+/**
+ *
+ * Pass this in when calling api
+ *
+ */
+export type Params = {
+  q?: query;
+  chart?: "mostPopular";
+  regionCode?: "US" | "IN";
+};
+
+/**
+ *
+ * How data from the api call should return
+ *
+ */
+export type ResponseItem = {
+  id: { kind: string; videoId: string };
+  snippet: any;
+};
+
+export type Thumbnail = {
+  url: string;
+  width: number;
+  height: number;
+};
+
+/**
+ *
+ * The base format for using in a component
+ *
+ */
+export interface Result {
+  kind: string;
+  title: string;
+  videoId: string;
+  channelId: string;
+  channelTitle: string;
+  description: string;
+  publishTime: string;
+  publishedAt: string;
+  liveBroadcastContent: string;
+  thumbnails: { default: Thumbnail; high: Thumbnail; medium: Thumbnail };
+}
